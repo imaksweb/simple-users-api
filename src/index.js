1ad790a2c382a5a21107dfad6ea2739c99cc55ad
Ilya Maksymov <ivashthestampede01@gmail.com>
2023-02-08 09:55:18 +0200

    Setup cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 import { authRouter } from './routes/authRouter.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
 app.use(express.json());
 app.use(authRouter);
 
