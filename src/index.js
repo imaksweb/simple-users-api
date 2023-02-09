de46bb9348eddff4395a354d2add718c0d257d2a
Ilya Maksymov <ivashthestampede01@gmail.com>
2023-02-09 12:27:17 +0200

    Implement error handling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import { authRouter } from './routes/authRouter.js';
+import { errorMiddleware } from './middlewares/errorMiddleware.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,9 +14,7 @@ app.use(cors({
 app.use(express.json());
 app.use(authRouter);
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-})
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Listening on port http://localhost:${PORT}`);
